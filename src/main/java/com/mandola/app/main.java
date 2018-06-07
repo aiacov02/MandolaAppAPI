33aa6fccb8ddf110fd49cf5f26126da56d17b2ae
Andreas Iacovou <aiacov02@ucy.ac.cy>
2018-06-08 00:31:00 +0300

    Update main.java

--- src/main/java/com/mandola/app/main.java
+++ src/main/java/com/mandola/app/main.java
@@ -1,7 +1,7 @@
 package com.mandola.app;
 
 import com.google.gson.Gson;
-import org.tc33.jheatchart.HeatChart;
+// import org.tc33.jheatchart.HeatChart;
 
 import java.awt.*;
 import java.io.File;
@@ -33,56 +33,20 @@ public class main {
 
         });
 
-        get("/getTwitterReports", (request, response) -> {
-            response.type("application/json");
+//         get("/getTwitterReports", (request, response) -> {
+//             response.type("application/json");
 
 
-            try {
-                StatisticsController.RetrieveTweets();
+//             try {
+//                 StatisticsController.RetrieveTweets();
 
-                return "200";
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "500";
-            }
+//                 return "200";
+//             } catch (Exception e) {
+//                 e.printStackTrace();
+//                 return "500";
+//             }
 
-        });
-
-        get("/test", (request, response) -> {
-            response.type("application/json");
-
-
-            double[][] data = new double[][]{{3,2,3,4,5,6},
-                    {2,3,4,5,6,7},
-                    {3,4,5,6,7,6},
-                    {4,5,6,7,6,5}};
-
-            // Step 1: Create our heat map chart using our data.
-            HeatChart map = new HeatChart(data);
-
-            // Step 2: Customise the chart.
-            map.setTitle("This is my heat chart title");
-            map.setXAxisLabel("X Axis");
-            map.setYAxisLabel("Y Axis");
-            map.setLowValueColour(white);
-            map.setHighValueColour(blue);
-            map.setXValues(new Object[]{"Religious","Gender","Sexual","Class","Politics","Ethnicity","Nationality","Other"});
-            map.setXValuesHorizontal(false);
-            map.setYValues(new Object[]{});
-            map.setXAxisLabel("Categories");
-            map.setYAxisLabel("Time Periods");
-            Dimension dimension = new Dimension();
-            dimension.height = 50;
-            dimension.width=50;
-            map.setCellSize(dimension);
-
-
-            // Step 3: Output the chart to a file.
-            map.saveToFile(new File("java-heat-chart.png"));
-
-            return "200";
-
-        });
+//         });
 
 //        get("/CalculateStatistics", (request, response) -> {
 //            response.type("application/json");
